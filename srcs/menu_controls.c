07632bbfd290bdf5c90c20f8edce46fb5a97814d
Adriano RUIZ BARBERO <aruiz-ba@e2r13p7.42.fr>
2020-01-24 14:43:24 +0100

    works

--- srcs/menu_controls.c
+++ srcs/menu_controls.c
@@ -6,7 +6,7 @@
 /*   By: aruiz-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 15:53:31 by aruiz-ba          #+#    #+#             */
-/*   Updated: 2020/01/23 18:01:09 by aruiz-ba         ###   ########.fr       */
+/*   Updated: 2020/01/24 14:34:45 by aruiz-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,39 @@ void			ft_print_buttons(t_fdf *img)
 
 }
 
-void			ft_menu_event(t_map *map, t_mouse *mous, SDL_Event e)
+void			loop_til_click()
+{
+	int			isquit;
+	SDL_Event	e;
+
+	isquit = 0;
+	while (isquit == 0)
+	{
+		if (SDL_PollEvent(&e)) 
+		{
+			if (e.type == SDL_QUIT || e.key.keysym.sym == SDLK_ESCAPE)
+				exit (0);
+			if (e.type == SDL_MOUSEBUTTONDOWN)
+				break;
+		}
+	}
+	
+}
+
+void			ft_menu_event(t_map *map, t_mouse *mous, SDL_Event e, t_wlist *wlst)
 {
 	if (e.type == SDL_MOUSEBUTTONDOWN)
 	{
 		if (e.button.button == SDL_BUTTON_LEFT)
 		{
-			//SDL_GetMouseState(&mous->click1[0], &mous->click1[1]); // needs to be uncommented once this is fixed
-			/*
+			SDL_GetMouseState(&mous->click1[0], &mous->click1[1]); // needs to be uncommented once this is fixed
 			if (mous->click1[0] >= 1320 && mous->click1[0] <= 1550 && mous->click1[1] >= 120 && mous->click1[1] <= 170)
 			{
-				place_wall();
-				printf("cordsclicked: (%i, %i)\n", mous->click1[0], mous->click1[1]);
-			} */
+				//place_wall();
+				loop_til_release();
+				loop_til_click();
+				ft_mouse_event(map, mous, e, &wlst);
+			} 
 			//every if checks if you click on every button and if you do it runs wall placing button, objects and enemy placing 
 			//or undo/remove/move elements
 		}
