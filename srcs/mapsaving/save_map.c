9b329f41e928923cf1f7c6d474f671c6f989c560
Adriano RUIZ BARBERO <aruiz-ba@e1r4p8.42.fr>
2020-01-29 18:10:30 +0100

    save button cretes a file and writes inside of it

--- srcs/mapsaving/save_map.c
+++ srcs/mapsaving/save_map.c
@@ -6,7 +6,7 @@
 /*   By: aruiz-ba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 17:09:34 by aruiz-ba          #+#    #+#             */
-/*   Updated: 2020/01/29 17:17:21 by aruiz-ba         ###   ########.fr       */
+/*   Updated: 2020/01/29 18:08:01 by aruiz-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	ft_save_map(t_wlist **wlst, t_olist **olst)
 {
+	FILE *file = NULL;
+	char *data;
 
+	file = fopen("DOOM-map", "w");
+
+	if (file == NULL)
+	{
+		ft_putstr("Unable to create file.\n");
+		exit(0);
+	}
+	data = "W9.8.10.15W8.7.20.21";
+	fputs(data, file);
+	fclose(file);
+	ft_putstr("File created succesfully\n");
+	exit(0);
 }
